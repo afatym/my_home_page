6af9a8871ec01d1be1357249da0cd98e27b4bb90
afatym <93615086+afatym@users.noreply.github.com>
2022-10-07 10:03:22 +0300

    Delete hanoi directory

--- hanoi/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("ƒобро пожаловать в ханойские башни. ¬ведите количество колец ( не меньше 3х):");
-        Game game = new Game(scanner.nextInt());
-    }
-}
